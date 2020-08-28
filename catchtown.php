76583b0ee4f6016611a28bbb680d3bcf0af249be
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-28 11:38:34 +0800

    關閉抓取初始資料程式碼

--- catchtown.php
+++ catchtown.php
@@ -1,4 +1,5 @@
 <?php
+exit();
 $json_url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-6DBF737A-A676-4BC9-A35B-334E259FB4D2";
 $json = file_get_contents($json_url);
 $links = json_decode($json, TRUE);
