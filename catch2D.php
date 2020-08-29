2e6fa08c74248898180a2b46a8fa48cc3aab2f94
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-29 09:30:26 +0800

    研究API結構

--- catch2D.php
+++ catch2D.php
@@ -4,6 +4,31 @@ $json = file_get_contents($json_url);
 $links = json_decode($json, TRUE);
 $i = 0;
 
-echo $links['records']['locations'][0]['location'][0]['locationName'];
+
+foreach($links['records']['locations'][0]['location'] as $key => $val){
+    echo $val['locationName'] . '<br>';
+
+    echo $val['weatherElement'][0]['description'] . '<br>';
+    foreach($val['weatherElement'][0]['time'] as $key2 => $val2){
+        echo $val2['startTime'] . '<br>';
+        echo $val2['elementValue'][0]['value'] . '<br>';
+        $startTime = $val2['startTime'];
+
+        foreach($val['weatherElement'][1]['time'] as $key3 => $val3){
+            if($val3['startTime'] == $startTime){
+                echo $val3['elementValue'][0]['value'] . '<br>';
+                echo $val3['elementValue'][1]['value'] . '<br>';
+            }
+        }
+
+        foreach($val['weatherElement'][3]['time'] as $key4 => $val4){
+            if($val4['dataTime'] == $startTime){
+                echo $val4['elementValue'][0]['value'] . '<br>';
+            }
+        }
+    }
+
+    echo "<hr>";
+}
 ?>
 
