14eceb07eb1c13c06178d050b657d68ccbbb49f0
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-09-07 09:49:06 +0800

    MVC : 建模

---
+++ MVCmod/controllers/helloController.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once './models/helloModel.php';
+
+class helloC {
+
+}
+
+?>
---
+++ MVCmod/models/helloModel.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once './models/database.php';
+
+class helloM extends database{
+
+}
+
+?>
